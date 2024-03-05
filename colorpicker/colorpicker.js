633b672f3ef4a04129615d9a52dfa8dd2ac571a6
practice <sivaramakrishnapilli2@gmail.com>
2024-03-06 00:38:19 +0530

    first practice

---
+++ colorpicker/colorpicker.js
@@ -0,0 +1,26 @@
+let bgCard = document.getElementById("mainCard");
+let result = document.getElementById("colorChanger");
+
+function changeColor1() {
+    bgCard.style.backgroundColor = "#e0e0e0";
+    result.textContent="#e0e0e0";
+
+}
+
+function changeColor2() {
+    bgCard.style.backgroundColor = "#6fcf97";
+    result.textContent="#6fcf97";
+
+}
+
+function changeColor3() {
+    bgCard.style.backgroundColor  = "#56ccf2";
+    result.textContent="#56ccf2";
+
+}
+
+function changeColor4() {
+    bgCard.style.backgroundColor = "#bb6bd9";
+    result.textContent="#bb6bd9";
+
+}
